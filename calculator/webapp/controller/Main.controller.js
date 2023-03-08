aaf38158837c3287bf406f4f5ab71975fdc896e0
Kaan Can Calkan <cancalkaniletisim@gmail.com>
2023-03-08 06:12:35 +0300

    Added JSON Model

--- calculator/webapp/controller/Main.controller.js
+++ calculator/webapp/controller/Main.controller.js
@@ -1,21 +1,43 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller",  "sap/ui/model/json/JSONModel"],
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
  
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller,JSONModel) {
+  function (Controller,JSONModel,Fragment) {
     "use strict";
 
     return Controller.extend("fiori.calculator.controller.Main", {
-      onInit: function () {},
+      onInit: function () {
+        var oCalculationData ={
+            input1:'1',
+            input2:'3',
+            operator:'+',
+            result:'',
+            aSelect: [{
+                key:'+'
+            },
+            {
+                key:'-'
+            },
+            {
+                key:'/'
+            },
+            {
+                key:'*'
+            }]
+        },
+        oModel = new JSONModel(oCalculationData);
+        this.getView().setModel(oModel);
+      },
       calculate: function () {
-        var input1 = this.byId("input1"),
-          input2 = this.byId("input2"),
-          operator = this.byId("Operator"),
-          sValue = parseInt(input1.getValue(),10),
-          sValue2 = parseInt(input2.getValue(),10),
-          sOperator = operator.getSelectedKey(),
+        var oModel =this.getView().getModel(),
+        // input1 = this.byId("input1"),
+        //   input2 = this.byId("input2"),
+        //   operator = this.byId("Operator"),
+          sValue = parseInt(oModel.getProperty("/input1"),10),
+          sValue2 = parseInt(oModel.getProperty("/input2"),10),
+          sOperator = oModel.getProperty("/operator"),
           iResult = 0;
         switch(sOperator){
             case '+':
@@ -31,8 +53,19 @@ sap.ui.define(
                 iResult = sValue * sValue2;
             break ;      
         }
-        alert("The Result is" + iResult);
+        oModel.setProperty("/result", iResult);
+        // alert("The Result is" + iResult);
       },
+      formatResult: function(iResult){
+        var oBundle = this.getView().getModel("i18n") && this.getView().getModel("i18n").getResourceBundle();
+        if (oBundle) {
+          var sResult = oBundle.getText("appResult", [iResult]);
+          if (iResult) {
+            return sResult;
+          }
+        }
+        return "";
+      }
     });
   }
 );
